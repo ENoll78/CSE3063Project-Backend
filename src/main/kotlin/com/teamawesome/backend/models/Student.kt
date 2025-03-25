53f2671a68c344805dc9a571efff7db5ca7940c5
ENoll78 <ErikNoll78@gmail.com>
2025-03-24 23:54:37 -0600

    add Student user-type

---
+++ src/main/kotlin/com/teamawesome/backend/models/Student.kt
@@ -0,0 +1,52 @@
+package com.teamawesome.backend.models
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.ObjectIdGenerators
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Embeddable
+import jakarta.persistence.Entity
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotEmpty
+
+/**
+ *
+ * Student user, extends User fields
+ * Stores a list of courses with their current grades, compute overall GPA?
+ *
+ */
+
+@Entity
+@JsonIdentityInfo(
+    generator = ObjectIdGenerators.PropertyGenerator::class,
+    property = "id")
+class Student : User() {
+
+    /**
+     * A list of course records for this student, each containing a course name and grade.
+     * This is read-only for students (enforced at the service/controller layer),
+     * but faculty can modify grades for their classes.
+     */
+    @ElementCollection
+    var courses: MutableList<CourseGrade> = mutableListOf()
+
+    val gpa: Double
+        get() = if (courses.isEmpty()) 0.0 else courses.map {it.grade}.average()
+
+}
+
+/**
+ *
+ * Embeddable type for a single course and it's grade
+ */
+@Embeddable
+data class CourseGrade(
+    // Name of a course
+    @NotEmpty
+    var courseName: String = "",
+
+    // Grade
+    @Min(0)
+    @Max(4)
+    var grade: Double = 0.0,
+)
