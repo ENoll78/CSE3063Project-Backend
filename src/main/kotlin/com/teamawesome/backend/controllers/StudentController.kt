35b09cba4fa5e2cdeb8376ed510639fa55f45431
ENoll78 <ErikNoll78@gmail.com>
2025-03-26 14:50:59 -0600

    I was bored during lab

---
+++ src/main/kotlin/com/teamawesome/backend/controllers/StudentController.kt
@@ -0,0 +1,30 @@
+package com.teamawesome.backend.controllers
+
+import com.teamawesome.backend.repositories.StudentRepository
+import com.teamawesome.backend.services.StudentService
+import com.teamawesome.backend.models.Student
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ *  Handles front end service requests to @Services layer
+ *
+ *  Most of this will just be mapping requests
+ */
+
+@RestController
+@CrossOrigin()
+class StudentController {
+
+    // Get by id
+
+    // Get by OAuth2 id
+
+    // Get by Username
+
+    // Get Courses + Grades
+
+}
